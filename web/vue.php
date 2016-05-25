8d1123fd7ee04d31f5e0b96d2ece228fad4e3db9
BuildTools <unconfigured@null.spigotmc.org>
2016-05-25 12:05:48 +0200

    Ajout controlleur

---
+++ web/vue.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Navision</title>
+
+	<!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	<!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+	<script>  $( document ).ready(function(){  $(".button-collapse").sideNav(); }) </script>
+	<nav>
+		<div class="nav-wrapper">
+            <a href="" class="brand-logo center">Navision</a>
+			<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="">Où suis-je ?</a></li>
+                <li><a href="">Points d'intérêts</a></li>
+                <li><a href="">Plan de l'école</a></li>
+            </ul>
+		</div>
+	</nav>
+</body>
+</html>
