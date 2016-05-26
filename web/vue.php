54155287ac6661b93bef597bd6bec93a94e21db3
SwagPanda <antoine.blondeau@isen-bretagne.fr>
2016-05-26 10:02:57 +0200

    Retrait framework JS html5-qrcode (non fonctionnel)

--- web/vue.php
+++ web/vue.php
@@ -16,7 +16,14 @@
 	<!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
-	<script>  $( document ).ready(function(){  $(".button-collapse").sideNav(); }) </script>
+    
+	<script>  
+        $( document ).ready(
+        function()
+        {  
+            $(".button-collapse").sideNav();
+        });
+    </script>
 	<nav>
 		<div class="nav-wrapper">
             <a href="" class="brand-logo center">Navision</a>
