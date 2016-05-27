01d31e653ff513dc1f239ee7d174518eebe67ba1
SwagPanda <antoine.blondeau@isen-bretagne.fr>
2016-05-27 08:57:43 +0200

    Implementation fonctionnelle de QRcode reader

--- web/index.php
+++ web/index.php
@@ -1,9 +1,11 @@
 <?php
+	// Initialisation
 	$bdd = new PDO('mysql:host=localhost;dbname=Navision;charset=utf8', 'Navision', 'Navision');
-	include_once('vue.php');
 	include_once('modele.php');
 	include_once('dijkstra.php');
 	$points = get_points();
 	$liaisons = get_liaisons();
 
+	include_once('vue.php');
+	//include_once('scan.php');
 ?>
---
+++ web/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+        $(".button-collapse").sideNav();
+        $('#reader').html5_qrcode(function(data)
+        {
+            $('#read').html(data);
+            //alert("success, link is :"+data);
+            location.href = data;
+        },
+        function(error){
+            $('#read_error').html(error);
+
+        },
+        function(videoError){
+            $('#read_videoError').html(videoError);
+        });
+});
