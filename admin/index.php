1154297a8e02dad3d3d374bb82cbb893a8cdeaa2
BuildTools <unconfigured@null.spigotmc.org>
2016-05-30 08:54:15 +0200

    ajout déconnexion

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,15 @@
 <?php
+	if(!isset($_SESSION))session_start();
+	if(!isset($_SESSION['login']))$_SESSION=FALSE;
 	$bdd = new PDO('mysql:host=localhost;dbname=Navision;charset=utf8', 'Navision', 'Navision');
-	include_once 'vue.php';
 	include_once 'modele.php';
+	include_once 'login.php';
+	if(isset($_POST['user']) && isset($_POST['password'])){
+		$_SESSION['login']=login(get_users());
+	}
+	if($_SESSION['login'])include_once 'vue/admin.php';
+	else include_once 'vue/vue.php';
 ?>
+<pre>
+	<?php print_r($_SESSION)?>
+</pre>
--- web/index.php
+++ web/index.php
@@ -1,8 +1,9 @@
 <?php
-	$bdd = new PDO('mysql:host=home.apremel.fr;dbname=Navision;charset=utf8', 'Navision', 'Navision');
+	$bdd = new PDO('mysql:host=localhost;dbname=Navision;charset=utf8', 'Navision', 'Navision');
 	//include_once('vue.php');
-	include_once('modele.php');
+//	include_once('modele.php');
 	include_once('dijkstra.php');
+	include_once('../admin/modele.php');
 	$points = get_points();
 	$liaisons = get_liaisons();
 ?>
