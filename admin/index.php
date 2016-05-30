bfbabe1ab5891e465675e34ac00db6222c0bba84
BuildTools <unconfigured@null.spigotmc.org>
2016-05-30 15:22:59 +0200

    Ajout de la partie édition des points

--- admin/index.php
+++ admin/index.php
@@ -4,10 +4,20 @@
 	$bdd = new PDO('mysql:host=localhost;dbname=Navision;charset=utf8', 'Navision', 'Navision');
 	include_once 'modele.php';
 	include_once 'login.php';
-	if(isset($_POST['user']) && isset($_POST['password'])){
-		$_SESSION['login']=login(get_users());
+	if(isset($_GET['action'])){
+		unset($_SESSION['login']);
+		switch ($_GET['action']){
+			case 'plan':
+			include_once 'vue/plans.php';
+			break;
+		}
 	}
-	if($_SESSION['login'])include_once 'vue/admin.php';
-	else include_once 'vue/vue.php';
+	else{
+		if(isset($_POST['user']) && isset($_POST['password'])){
+			$_SESSION['login']=login(get_users());
+		}
+		if($_SESSION['login'])include_once 'vue/admin.php';
+		else include_once 'vue/vue.php';
+	}
 ?>
 		<?php //print_r($_SESSION)?>
